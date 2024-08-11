6cf48d20a0c64fc9c6f820f8f2b916ace32eb32f
antigluon <optionaloctave@gmail.com>
2024-08-11 17:28:56 -0400

    emulator initialization and program load

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,39 @@
 const rl = @import("raylib");
 const std = @import("std");
 
+const display_size = 64 * 32 / 8;
+
 const Emulator = struct {
     memory: [4096]u8,
-    display: std.bit_set.ArrayBitSet(u8, 64 * 32),
+    display: [display_size]u8,
     pc: u16,
     index: u16,
     stack: std.ArrayList(u16),
+    registers: [16]u8,
     delay_timer: u8,
     sound_timer: u8,
-    registers: [16]u8,
+
+    fn new() Emulator {
+        var stack_mem = [_]u8{0} ** 16;
+        var stack_alloc = std.heap.FixedBufferAllocator.init(&stack_mem);
+        return Emulator{
+            .memory = [_]u8{0x00} ** 4096,
+            .display = [_]u8{0x00} ** display_size,
+            .registers = [_]u8{0x00} ** 16,
+            .pc = 0x200,
+            .index = 0,
+            .delay_timer = 0,
+            .sound_timer = 0,
+            .stack = std.ArrayList(u16).init(stack_alloc.allocator()),
+        };
+    }
+
+    fn load_program(self: *Emulator, program: []const u8) !void {
+        if (program.len > self.memory.len - 0x200) {
+            return error.OutofMemory;
+        }
+        std.mem.copyForwards(u8, self.memory[(0x200)..], program);
+    }
 };
 
 pub fn main() !void {
@@ -26,4 +50,17 @@ pub fn main() !void {
         rl.clearBackground(rl.Color.white);
         rl.drawText("Work in progress!", screenWidth / 2 - 100, screenHeight / 2 - 20, 20, rl.Color.black);
     }
+}
+
+const expect = std.testing.expect;
+test "program initialization" {
+    var emulator = Emulator.new();
+    const ones: [0x200]u8 = [_]u8{0xFF} ** 0x200;
+    try emulator.load_program(&ones);
+    try expect(emulator.memory[0x000] == 0x00);
+    try expect(emulator.memory[0x100] == 0x00);
+    try expect(emulator.memory[0x200] == 0xFF);
+    try expect(emulator.memory[0x201] == 0xFF);
+    try expect(emulator.memory[0x3FF] == 0xFF);
+    try expect(emulator.memory[0x400] == 0x00);
 }
