bee4940defd294968057b007d4c2e1d41784a600
antigluon <optionaloctave@gmail.com>
2024-08-11 21:09:05 -0400

    basic instruction decode

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,37 @@ const std = @import("std");
 
 const display_size = 64 * 32 / 8;
 
+const Opcode = enum(u4) {
+    ClearScreen = 0x0,
+    Jump = 0x1,
+    Set = 0x6,
+    Add = 0x7,
+    SetIndex = 0xA,
+    Draw = 0xD,
+};
+
+fn decode_instruction(instructionCode: u16) !Instruction {
+    const opcode: u4 = @truncate(instructionCode >> 12);
+    return switch (opcode) {
+        0x0 => .{ .ClearScreen = {} },
+        0x1 => .{ .Jump = .{ .dest = @truncate(instructionCode) } },
+        0x6 => .{ .Set = .{ .reg = @truncate(instructionCode >> 8), .val = @truncate(instructionCode) } },
+        0x7 => .{ .Add = .{ .reg = @truncate(instructionCode >> 8), .val = @truncate(instructionCode) } },
+        0xA => .{ .SetIndex = .{ .val = @truncate(instructionCode) } },
+        0xD => .{ .Draw = .{ .regX = @truncate(instructionCode >> 8), .regY = @truncate(instructionCode >> 4), .height = @truncate(instructionCode) } },
+        else => error.InvalidInput,
+    };
+}
+
+const Instruction = union(enum) {
+    ClearScreen,
+    Jump: struct { dest: u12 },
+    Set: struct { reg: u4, val: u8 },
+    Add: struct { reg: u4, val: u8 },
+    SetIndex: struct { val: u12 },
+    Draw: struct { regX: u4, regY: u4, height: u4 },
+};
+
 const Emulator = struct {
     memory: [4096]u8,
     display: [display_size]u8,
@@ -42,8 +73,8 @@ const Emulator = struct {
 };
 
 pub fn main() !void {
-    const screenWidth = 800;
-    const screenHeight = 450;
+    const screenWidth = 1280;
+    const screenHeight = 640;
 
     rl.initWindow(screenWidth, screenHeight, "ZCrisp Emulator");
     defer rl.closeWindow();
@@ -53,6 +84,7 @@ pub fn main() !void {
     var recentFrameTimes: [20]f32 = [_]f32{0.0} ** 20;
     var frameClock: u8 = 0;
     var frameTimeBuf: [32]u8 = undefined;
+    var recentFramesTotal: f32 = 0.0;
     var frameTimeText: []u8 = "";
 
     var emulator = Emulator.new();
@@ -68,10 +100,11 @@ pub fn main() !void {
         frameClock +%= 1;
         const time = rl.getTime();
         const deltaTime = rl.getFrameTime() * 1000; // milliseconds
-        recentFrameTimes[frameClock % 32] = deltaTime;
+        recentFramesTotal += deltaTime;
+        recentFramesTotal -= recentFrameTimes[frameClock % 20];
+        recentFrameTimes[frameClock % 20] = deltaTime;
         if (time > 0.6 and frameClock % 8 == 0) {
-            const averageFrameTime = std.math.average(recentFrameTimes);
-            frameTimeText = try std.fmt.bufPrint(&frameTimeBuf, "Frame time: {d:.2} ms\n", .{averageFrameTime});
+            frameTimeText = try std.fmt.bufPrint(&frameTimeBuf, "Frame time: {d:.2} ms\n", .{recentFramesTotal / 20});
             frameTimeText[frameTimeText.len - 1] = 0;
         }
         rl.drawText(@ptrCast(frameTimeText), 10, 10, 20, rl.Color.dark_gray);
@@ -113,4 +146,25 @@ test "timer tick" {
     emulator.tick_timers();
     try expect(emulator.delay_timer == 0);
     try expect(emulator.sound_timer == 0);
+}
+
+test "instruction decode" {
+    const clear = try decode_instruction(0x0E0);
+    try expect(clear.ClearScreen == {});
+
+    const jump = try decode_instruction(0x1200);
+    try expect(jump.Jump.dest == 0x200);
+
+    const set = try decode_instruction(0x64F0);
+    try expect(set.Set.reg == 0x4);
+    try expect(set.Set.val == 0xF0);
+
+    const add = try decode_instruction(0x780F);
+    try expect(add.Add.reg == 0x8);
+    try expect(add.Add.val == 0x0F);
+
+    const draw = try decode_instruction(0xD238);
+    try expect(draw.Draw.regX == 0x2);
+    try expect(draw.Draw.regY == 0x3);
+    try expect(draw.Draw.height == 0x8);
 }
